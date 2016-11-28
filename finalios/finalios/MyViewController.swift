3e0692da703336a68a2e738cc6b0249e6a028475
Mjb187UA <mjb187@zips.uakron.edu>
2016-11-28 13:30:30 -0500

    GamePage
    created the game page using a view controller and a page view controller

---
+++ finalios/finalios/MyViewController.swift
@@ -0,0 +1,134 @@
+//
+//  MyViewController.swift
+//  finalios
+//
+//  Created by Larry Baker on 11/27/16.
+//  Copyright © 2016 Matthew Baker. All rights reserved.
+//
+
+import UIKit
+
+class MyViewController: UIViewController{
+
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var button4: UIButton!
+    @IBOutlet weak var wrongLabel: UILabel!
+    @IBOutlet weak var rightLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    var answer1Text: String = ""
+    var answer2Text: String = ""
+    var answer3Text: String = ""
+    var answer4Text: String = ""
+    var correctText: String = ""
+    var correct: Int = 0
+    var wrong: Int = 0
+    var pageIndex: Int = 0;
+    var question: String = ""
+    var counter: Int = 60
+
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        var timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(MyViewController.updateTimer), userInfo: nil, repeats: true)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.questionLabel.text = question
+        self.wrongLabel.text = "Wrong: \(wrong)"
+        self.rightLabel.text = "Correct: \(correct)"
+        self.button1!.setTitle(answer1Text, for: UIControlState.normal)
+        self.button2!.setTitle(answer2Text, for: UIControlState.normal)
+        self.button3!.setTitle(answer3Text, for: UIControlState.normal)
+        self.button4!.setTitle(answer4Text, for: UIControlState.normal)
+        self.timeLabel.text = String(counter)
+        
+    }
+    
+
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func button1next(_ sender: Any) {
+        let pvc = self.parent as! gamePageViewController
+        let answer = button1.currentTitle;
+        if answer == correctText{
+            pvc.right += 1
+        }
+        else{
+            pvc.wrong += 1
+        }
+
+        pvc.counter = counter
+        pvc.nextPageWithIndex(index: pageIndex)
+    }
+    @IBAction func button2next(_ sender: Any) {
+        let pvc = self.parent as! gamePageViewController
+        let answer = button2.currentTitle;
+        if answer == correctText{
+            pvc.right += 1
+        }
+        else{
+            pvc.wrong += 1
+        }
+        
+        pvc.counter = counter
+        pvc.nextPageWithIndex(index: pageIndex)
+    }
+    
+    @IBAction func button3next(_ sender: Any) {
+        let pvc = self.parent as! gamePageViewController
+        let answer = button3.currentTitle;
+        if answer == correctText{
+            pvc.right += 1
+            print("\(pageIndex)")
+        }
+        else{
+            pvc.wrong += 1
+            print("\(pageIndex)")
+        }
+        
+        pvc.counter = counter
+        pvc.nextPageWithIndex(index: pageIndex)
+    }
+    @IBAction func button4next(_ sender: Any) {
+        let pvc = self.parent as! gamePageViewController
+        let answer = button4.currentTitle;
+        if answer == correctText{
+            pvc.right += 1
+        }
+        else{
+            pvc.wrong += 1
+        }
+        
+        pvc.counter = counter
+        pvc.nextPageWithIndex(index: pageIndex)
+    }
+    
+    func updateTimer()
+    {
+        counter -= 1
+        timeLabel.text = String(counter)
+    }
+}
---
+++ finalios/finalios/gamePageViewController.swift
@@ -0,0 +1,120 @@
+//
+//  gamePageViewController.swift
+//  finalios
+//
+//  Created by Larry Baker on 11/27/16.
+//  Copyright © 2016 Matthew Baker. All rights reserved.
+//
+
+import UIKit
+
+class gamePageViewController: UIPageViewController, UIPageViewControllerDataSource  {
+
+    var pvc: UIPageViewController!
+    var test: NSArray = NSArray()
+    var answer1: [String] = []
+    var answer2: [String] = []
+    var answer3: [String] = []
+    var answer4: [String] = []
+    var correct: [String] = []
+    var right: Int = 0
+    var wrong: Int = 0
+    var counter: Int = 60
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let Qpath = Bundle.main.path(forResource: "questionList", ofType: "plist")
+        let questionArray = NSArray(contentsOfFile: Qpath!)
+        let A1path = Bundle.main.path(forResource: "Answer1List", ofType: "plist")
+        let answer1Array = NSArray(contentsOfFile: A1path!)
+        let A2path = Bundle.main.path(forResource: "Answer2List", ofType: "plist")
+        let answer2Array = NSArray(contentsOfFile: A2path!)
+        let A3path = Bundle.main.path(forResource: "Answer3List", ofType: "plist")
+        let answer3Array = NSArray(contentsOfFile: A3path!)
+        let A4path = Bundle.main.path(forResource: "Answer4List", ofType: "plist")
+        let answer4Array = NSArray(contentsOfFile: A4path!)
+        let CApath = Bundle.main.path(forResource: "CorrectAnswerList", ofType: "plist")
+        let correctAnswerArray = NSArray(contentsOfFile: CApath!)
+        
+        
+        test = questionArray as! [String] as NSArray
+        answer1 = answer1Array as! [String]
+        answer2 = answer2Array as! [String]
+        answer3 = answer3Array as! [String]
+        answer4 = answer4Array as! [String]
+        correct = correctAnswerArray as! [String]
+        
+        self.dataSource = self
+        self.setViewControllers([getViewControllerAtIndex(index: 0)] as [UIViewController], direction: UIPageViewControllerNavigationDirection.forward, animated: false, completion: nil)
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    
+    func getViewControllerAtIndex(index: NSInteger) -> MyViewController
+    {
+        // Create a new view controller and pass suitable data.
+        let myViewController = storyboard?.instantiateViewController(withIdentifier: "MyViewController") as! MyViewController
+        myViewController.question = "\(test[index])"
+        myViewController.answer1Text = self.answer1[index]
+        myViewController.answer2Text = self.answer2[index]
+        myViewController.answer3Text = self.answer3[index]
+        myViewController.answer4Text = self.answer4[index]
+        myViewController.correctText = self.correct[index]
+        myViewController.pageIndex = index
+        myViewController.correct = right
+        myViewController.wrong = wrong
+        myViewController.counter = counter
+        return myViewController
+    }
+    
+    
+    func pageViewController(_ MyViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController?
+    {
+        let pageContent: MyViewController = viewController as! MyViewController
+        var index = pageContent.pageIndex
+        if ((index == 0) || (index == NSNotFound))
+        {
+            return nil
+        }
+        index -= 1;
+        return nil
+    }
+    
+    func pageViewController(_ MyViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController?
+    {
+        let pageContent: MyViewController = viewController as! MyViewController
+        var index = pageContent.pageIndex
+    
+        if (index == NSNotFound)
+        {
+            return nil;
+        }
+        index += 1;
+        if (index == test.count)
+        {
+            return nil;
+        }
+        
+        return nil
+    }
+    
+    func nextPageWithIndex(index : Int)
+    {
+        let nextVC = self.getViewControllerAtIndex(index: index+1)
+        setViewControllers([nextVC], direction: .forward, animated: true, completion: nil)
+    }
+
+    
+}
